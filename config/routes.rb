554ca86b35d5aa19b50464f48c0bcd1b449203b4
Jason Ho <jason.ho@obf.ateneo.edu>
2022-03-31 22:05:58 +0800

    Add routes for purchases

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
 
   get '/api/v1/cryptocurrencies', to: 'api/v1/cryptocurrencies#all'
   get '/api/v1/cryptocurrencies/:symbol', to: 'api/v1/cryptocurrencies#find'
+
+  get '/api/v1/purchases', to: 'api/v1/purchases#all'
+  post '/api/v1/purchases', to: 'api/v1/purchases#create'
 end
