a1722173c5a21e270f690aec41753259186a74cb
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-01 02:49:18 +0800

    Add uniquene combination validation to alerts

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -2,7 +2,14 @@ class Alert < ApplicationRecord
   validates :user_id, presence: true
   validates :cryptocurrency_id, presence: true
   validates :price, presence: true
-  validates :operator, presence: true, inclusion: { in: %w[higher lower] }
+  validates :operator,
+            presence: true,
+            inclusion: {
+              in: %w[higher lower],
+            },
+            uniqueness: {
+              scope: %i[user_id cryptocurrency_id price operator],
+            }
 
   belongs_to :user
   belongs_to :cryptocurrency
