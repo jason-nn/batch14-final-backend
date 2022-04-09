b5c0bee2422672b4cb038cd54c19f9ca506f1b80
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-09 20:51:39 +0800

    Update process token method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::API
         jwt_payload =
           JWT.decode(
             request.headers['Authorization'].split(' ')[1].remove('"'),
-            Rails.application.secrets.secret_key_base,
+            Rails.application.credentials.secret_key_base,
           ).first
         @current_user_id = jwt_payload['id']
       rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
