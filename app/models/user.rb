b08c2ec4972c3556952b855d4bf9bfaf505bbfa9
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-09 20:38:14 +0800

    Update generate jwt method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   def generate_jwt
     JWT.encode(
       { id: id, exp: 60.days.from_now.to_i },
-      Rails.application.secrets.secret_key_base,
+      Rails.application.credentials.secret_key_base,
     )
   end
 
